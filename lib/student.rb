d16e76a5e04bcb431fd961e1997fe0968e020f87
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-04 01:14:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+# #first_name
+#   has a first name (FAILED - 2)
+# #last_name
+#   has a last name (FAILED - 3)
+# #new
+#   initializes with an empty knowledge array (FAILED - 4)
+# #learn
+#   takes in an argument of a string of knowledge and adds it to the student's knowledge array (FAILED - 5)
+# #knowledge
+#   returns that student's knowledge array (FAILED - 6)
+#
+#
+class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 end
---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+# User
+#   has a first name (FAILED - 11)
+#   has a last name (FAILED - 12)
+
+
+class User
+  attr_accessor :first_name, :last_name
+
+end
